c87da486563a108788593232fcbbff4b2a889aa6
DongPL <dongleepham@gmail.com>
2026-01-13 21:33:24 +0700

    Fix WebSocket URL scheme for SockJS

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://103.200.21.203:8088/api/v1',
-  wsUrl: 'ws://103.200.21.203:8088/ws',
+  wsUrl: 'http://103.200.21.203:8088/ws',
 
   // Hoặc nếu dùng cùng domain với frontend:
   // apiBaseUrl: '/api/v1',
