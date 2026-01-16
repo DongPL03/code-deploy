18038133acab246c0ddf067c058e1806b24d10fb
DongPL <dongleepham@gmail.com>
2026-01-16 10:01:37 +0700

    Fix: Add @Transactional to CauHoiService to fix LazyInitializationException

--- backend/src/main/java/com/app/backend/services/cauhoi/CauHoiService.java
+++ backend/src/main/java/com/app/backend/services/cauhoi/CauHoiService.java
@@ -13,11 +13,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CauHoiService implements ICauHoiService {
     private final ICauHoiRepository cauHoiRepository;
     private final IBoCauHoiRepository boCauHoiRepository;
